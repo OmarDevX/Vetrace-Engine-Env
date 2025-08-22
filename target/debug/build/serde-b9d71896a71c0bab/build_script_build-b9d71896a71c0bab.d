35651c239789f9e9e972a02a16528c9a273b536d
Omar Dev <omardevxme@gmail.com>
2025-08-22 17:05:42 +0300

    bruh

---
+++ target/debug/build/serde-b9d71896a71c0bab/build_script_build-b9d71896a71c0bab.d
@@ -0,0 +1,5 @@
+/home/omar/Desktop/VetraceEngineENV/target/debug/build/serde-b9d71896a71c0bab/build_script_build-b9d71896a71c0bab.d: /home/omar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.219/build.rs
+
+/home/omar/Desktop/VetraceEngineENV/target/debug/build/serde-b9d71896a71c0bab/build_script_build-b9d71896a71c0bab: /home/omar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.219/build.rs
+
+/home/omar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.219/build.rs:
---
+++ target/debug/deps/smallvec-9312b5a7c8ab1b3c.d
@@ -0,0 +1,7 @@
+/home/omar/Desktop/VetraceEngineENV/target/debug/deps/smallvec-9312b5a7c8ab1b3c.d: /home/omar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs
+
+/home/omar/Desktop/VetraceEngineENV/target/debug/deps/libsmallvec-9312b5a7c8ab1b3c.rlib: /home/omar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs
+
+/home/omar/Desktop/VetraceEngineENV/target/debug/deps/libsmallvec-9312b5a7c8ab1b3c.rmeta: /home/omar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs
+
+/home/omar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs:
